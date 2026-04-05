d1024f17873f8abb5e3355f4a83dc337efb8cfb8
yokeshwaran01 <yokeshjayachandiran@gmail.com>
2026-04-05 03:56:46 +0000

    v1 release v1.0

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DevOps Pipeline v1🚀"
+    return "Hello from DevOps Pipeline v1.0🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
